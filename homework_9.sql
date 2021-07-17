a0cf84968d194d997e729e990f1c944720667fe9
Aycan Dindar <50072947+aycand@users.noreply.github.com>
2021-07-17 18:12:24 +0300

    Create homework_9.sql

---
+++ homework_9.sql
@@ -0,0 +1,18 @@
+SELECT city, country
+FROM country
+INNER JOIN city 
+ON city.country_id = country.country_id;
+
+---------------------------------------
+
+SELECT payment_id, first_name, last_name 
+FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id; 
+
+---------------------------------------
+
+SELECT first_name, last_name, rental_id 
+FROM customer
+INNER JOIN rental
+ON customer.customer_id = rental.customer_id;
